20d09fefdc724d20b9fc0c2496e680652a90b602
eva diaz <dev.evada@github.com>
2024-06-05 19:06:26 +0200

    se instala i18n  y se agrega varios archivos

---
+++ app/components/SideBar.tsx
@@ -0,0 +1,64 @@
+'use client'
+import React, { useState } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faBars, faBed, faCamera, faEnvelope, faHouse, faLocationDot, faXmark } from "@fortawesome/free-solid-svg-icons";
+import { FlagLanguage } from "./FlagLanguage";
+import { useTranslation } from "react-i18next";
+import Link from "next/link";
+
+export const SideBar: React.FC = () => {
+    const { t } = useTranslation();
+    const [ openModal, setOpenModal ] = useState(false);
+
+    const handleOpen = () => {
+        setOpenModal(true);
+    }
+
+    const handleClose = () => {
+        setOpenModal(false)
+    }
+
+    return(
+        <>
+        <div className="flex  flex-row justify-between items-center text-primary mx-4 rounded-full mt-2">
+            <FontAwesomeIcon icon={faBars} onClick={handleOpen}/>
+            <Link href="/home" className="flex flex-col items-center gap-1">
+                    <img className="w-12 h-12" src="/imgHostal.png" alt="logotipo" />
+                    <span className="font-bold">HOSTAL ANA</span>
+            </Link>
+            <div className="flex">
+                <FlagLanguage />
+            </div>
+        </div>
+            {openModal && (
+                <div className="absolute top-0 left-0 z-10 flex flex-col bg-slate-50 gap-10 w-full h-screen  text-primary">
+                    <button onClick={handleClose} className="flex items-center mt-5 ml-5 font-bold gap-1 hover:text-secundary">
+                    <FontAwesomeIcon icon={faXmark}  />
+                    <p>{t('cerrar')}</p>
+                    </button>
+                    <div className="flex items-center ml-5 gap-3 hover:text-secundary">
+                    <FontAwesomeIcon icon={faHouse} />
+                    <Link onClick={handleClose} href="/home" className="text-2xl">{t('inicio')}</Link>
+                    </div>
+                    <div className="flex items-center ml-5 gap-3 hover:text-secundary">
+                    <FontAwesomeIcon icon={faBed} />
+                    <Link onClick={handleClose} href="/room" className="text-2xl ">{t('habitaciones')}</Link>
+                    </div>
+                    <div className="flex items-center ml-5 gap-3 hover:text-secundary">
+                    <FontAwesomeIcon icon={faLocationDot} />
+                    <Link onClick={handleClose} href="/location" className="text-2xl ">{t('ubicacion')}</Link>
+                    </div>
+                    <div className="flex items-center ml-5 gap-3 hover:text-secundary">
+                    <FontAwesomeIcon icon={faCamera} />
+                    <Link onClick={handleClose} href="/gallery" className="text-2xl">{t('galeria')}</Link>
+                    </div>
+                    <div className="flex items-center ml-5 gap-3 hover:text-secundary">
+                    <FontAwesomeIcon icon={faEnvelope} />
+                    <Link onClick={handleClose} href="/contact" className="text-2xl ">{t('contacto')}</Link>
+                    </div>
+                </div> 
+            )}
+        </>
+
+    )
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,22 @@
-import Image from "next/image";
+'use client';
+import React from 'react';
+import { useTranslation } from 'react-i18next'
 
-export default function Home() {
-  
-  return (
-    <div>
-    <div className="static w-full h-96 bg-cover bg-no-repeat bg-center bg-[url(https://solaga.es/wp-content/uploads/2023/10/nerja-casco-antiguo-scaled-1.jpeg)]">
-    </div>
-    <article className="flex flex-col  p-5 gap-3 text-grey-ligth">
-        <p className="text-base md:text-xl">¡Bienvenido al Hostal Ana Nerja!</p>
-        <p>Sumérgete en la calidez y la comodidad de nuestro encantador hostal, ubicado en el corazón del maravilloso pueblo de Nerja. Nos enorgullece ofrecerte no solo un lugar donde descansar, sino una experiencia acogedora y familiar que te hará sentir como en casa desde el momento en que cruzas nuestras puertas.</p>
-        <p>Nuestra privilegiada ubicación te coloca a solo unos pasos de los principales atractivos de Nerja, desde sus pintorescas calles hasta sus hermosas playas de aguas cristalinas. Ya sea que estés aquí para explorar la rica cultura local, disfrutar del sol y la arena, o simplemente relajarte y desconectar, el Hostal Ana es el punto de partida perfecto para tus aventuras.</p>
-        <p>Nuestro equipo está comprometido en brindarte un servicio excepcional, siempre con una sonrisa y la calidez que caracteriza a los anfitriones de nuestra tierra. Desde consejos sobre los mejores lugares para visitar hasta recomendaciones de restaurantes locales, estamos aquí para que tu estancia sea inolvidable.</p>
-        <p>Bienvenido a tu hogar lejos de casa, donde cada momento será un recuerdo imborrable. ¡Esperamos que disfrutes tu estadía en el Hostal Ana Nerja!</p> 
-    </article>
-</div>
-  )
-;
+const HomePage = () => {
+    const { t } = useTranslation();
+    return (
+        <div>
+            <div className="static w-full h-96 bg-cover bg-no-repeat bg-center bg-[url(https://solaga.es/wp-content/uploads/2023/10/nerja-casco-antiguo-scaled-1.jpeg)]">
+            </div>
+            <article className="flex flex-col  p-5 gap-3 text-grey-ligth">
+                <p className="text-base md:text-xl">{t('textHome1')}</p>
+                <p>{t('textHome2')}</p>
+                <p>{t('textHome3')}</p>
+                <p>{t('textHome4')}</p>
+                <p>{t('textHome5')}</p> 
+            </article>
+        </div>
+    )
 }
+
+export default HomePage;
