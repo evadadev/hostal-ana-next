aaacb68497f84e966e15656598cb22b1bacb55dd
Raúl Rodríguez <raulrodfedev@gmail.com>
2024-06-06 18:35:59 +0200

    fix: se añade border a los inputs del formulario y se fija el foote en el fondo de la pantalla

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,8 +1,8 @@
 import React from "react";
 import type { Metadata } from "next";
 import { Header } from "./components/Header";
-import { Footer } from "./components/Footer"
-import './globals.css'
+import { Footer } from "./components/Footer";
+import "./globals.css";
 
 interface Props {
   children: React.ReactNode;
@@ -15,18 +15,24 @@ export const metadata: Metadata = {
 const RootLayout = (props: Props) => {
   const { children } = props;
 
+  const style = {
+    "min-height": "calc(100vh - 192px)",
+  };
+
   return (
     <html lang="es">
       <head>
-      <link rel="icon" type="image/svg+xml" href="/imgHostal.png" />
+        <link rel="icon" type="image/svg+xml" href="/imgHostal.png" />
       </head>
       <body>
         <Header />
-        <main className="min-h-mainScrren">{children}</main>
+        <main className="min-h-mainScrren" style={style}>
+          {children}
+        </main>
         <Footer />
       </body>
     </html>
   );
-}
+};
 
-export default RootLayout
+export default RootLayout;
